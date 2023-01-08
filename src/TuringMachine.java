f60ae21e513aae15673bcd5d48a041ae26acee9a
0CoolMichael <729963944@qq.com>
2023-01-08 14:02:09 +0800

    initial code

---
+++ src/TuringMachine.java
@@ -0,0 +1,85 @@
+
+public class TuringMachine {
+    int statenumber=0;
+    // Property to store the current position of the pointer
+    int pointer;
+    // Property to store the input string
+    String input;
+    // List to store the state objects
+
+    char[][] operations;
+    // Constructor to create a new Turing machine and set the number of states
+    public TuringMachine(int numStates,char[][] operations) {
+        this.pointer = 0;
+        this.input = "";
+        this.statenumber=numStates;
+        this.operations=operations;
+
+    }
+
+
+
+        public void processInput() {
+            // Set the current state to the first state in the list
+            int currentState = 0;
+            this.pointer = 1;
+
+            // Loop until an accept or reject state is reached
+            while (true) {
+                // Get the current character
+                char currentChar = this.input.charAt(this.pointer);
+
+                // Look up the next state, character to write, and direction to move the pointer
+                // in the state transition table
+                int nextState = -1;
+                char nextChar = ' ';
+                char direction = ' ';
+                for (char[] operation : this.operations) {
+                    if (Character.getNumericValue(operation[0]) == currentState
+                            && operation[1] == currentChar) {
+                        nextState = Character.getNumericValue(operation[4]);
+                        nextChar = operation[3];
+                        direction = operation[2];
+                        // Change the current state to the next state
+                        currentState = nextState;
+// Update the input string
+                        StringBuilder sb = new StringBuilder(this.input);
+                        sb.setCharAt(this.pointer , nextChar);
+                        this.input = sb.toString();
+// Move the pointer in the specified direction
+                        if (direction == 'L') {
+                            this.pointer--;
+                        } else if (direction == 'R') {
+                            this.pointer++;
+                        }
+                        break;
+                    }
+                }
+                if(pointer>=0){
+                System.out.println("The input is "+this.input+" and the next state " +
+                        "is "+nextState+", and the pointer points to the "+(this.pointer)
+                        +" bit of the string:"+this.input.charAt(pointer));
+                }
+                if (nextState == -1) {
+                    System.out.println("Rejected");
+                    break;
+                }
+
+                if (currentState == statenumber - 1) {
+                    // Current state is an accept state
+                    System.out.println("Accepted");
+                    break;
+                } else if (currentState == statenumber - 2) {
+                    // Current state is a reject state
+                    System.out.println("Rejected");
+                    break;
+                }
+            }
+        }
+
+
+}
+
+
+
+
---
+++ src/test.java
@@ -0,0 +1,38 @@
+public class test {
+    public static void main(String[] args) {
+        char[][] operations = new char[][] {
+                {'0', 'a', 'R', 'x', '1'},
+                {'0', 'x', 'R', 'x', '0'},
+                {'1', 'a', 'R', 'a', '1'},
+                {'1', 'b', 'R', 'y', '2'},
+                {'2', 'b', 'R', 'b', '2'},
+                {'2', 'z', 'R', 'z', '2'},
+                {'2', 'c', 'R', 'z', '5'},
+                {'3', 'y', 'R', 'y', '1'},
+                {'3', 'b', 'L', 'b', '3'},
+                {'4', 'y', 'L', 'b', '4'},
+                {'4', 'x', 'R', 'x', '0'},
+                {'4', 'a', 'L', 'a', '4'},
+                {'5', 'c', 'L', 'c', '5'},
+                {'5', 'z', 'L', 'z', '5'},
+                {'5', 'b', 'L', 'b', '3'},
+                {'5', 'y', 'L', 'b', '4'},
+                {'5', ' ', 'L', ' ', '6'},
+                {'6', 'a', 'R', 'a', '7'},
+                {'6', 'b', 'R', 'b', '7'},
+                {'6', 'x', 'L', 'x', '6'},
+                {'6', 'y', 'L', 'y', '6'},
+                {'6', 'z', 'L', 'z', '6'},
+                {'6', ' ', 'L', ' ', '8'},
+        };
+        TuringMachine tm = new TuringMachine(9,operations);
+
+// Set the input string
+        tm.input=" abccc ";
+
+
+// Process the input string
+        tm.processInput();
+    }
+
+}
